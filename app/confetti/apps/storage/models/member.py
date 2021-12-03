f72995b6e61a000eb9c2fe40c368a860152f39d9
Mezbaul Haque <mezba109@gmail.com>
2021-12-03 14:50:48 +0600

    Increase download_url and version length

--- app/confetti/apps/storage/models/member.py
+++ app/confetti/apps/storage/models/member.py
@@ -6,8 +6,8 @@ from confetti.apps.core.models import TimeStampedModel
 
 class MemberResume(TimeStampedModel):
     member = models.OneToOneField("core.User", on_delete=models.CASCADE, related_name="member_resume")
-    download_url = models.CharField(max_length=250, blank=True, null=True)
-    version = models.CharField(max_length=100, blank=True, null=True)
+    download_url = models.CharField(max_length=300, blank=True, null=True)
+    version = models.CharField(max_length=300, blank=True, null=True)
 
     class Meta:
         verbose_name = _("member résumé")
